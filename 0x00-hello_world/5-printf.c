8896431a45f4f4b18761c30b9a0873b91daf8481
Peter-Oenga <poenga230@gmail.com>
2023-06-15 17:37:50 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Code that prints a line of text using printf
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
