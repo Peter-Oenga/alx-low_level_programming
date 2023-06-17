189940a590d27fe2be0afb93fffbad196a85653c
Peter-Oenga <poenga230@gmail.com>
2023-06-17 09:10:47 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main - Display numbers of base10
+  *
+  *Return:Always 0 (Success)
+  */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
