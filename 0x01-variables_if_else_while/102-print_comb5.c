bfc9b06bc4ee849e6974cca45428194adc16ee28
Peter-Oenga <poenga230@gmail.com>
2023-06-19 17:21:12 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  *main - Print double number digits
+  *
+  *
+  *Return: Always 0 (Success)
+  *
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n <= 99; n++)
+	{
+		for (m = 0; m <= 99; m++)
+		{
+			putchar('0' + (n / 10));
+			putchar('0' + (n % 10));
+			putchar(' ');
+
+			putchar('0' + (m / 10));
+			putchar('0' + (m % 10));
+			if (n != 99 || m != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
