ce2e9a5116a8c48e4fe1f4e725487b2ec60a022b
Peter-Oenga <poenga230@gmail.com>
2023-06-23 17:49:50 -0700

    I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n);
+
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *print_diagonal - Displays a diagonal
+  *@n: Number of times to draw diagonal
+  *Return: nothing
+  *
+  */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
