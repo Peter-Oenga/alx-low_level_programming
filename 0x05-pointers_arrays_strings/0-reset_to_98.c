433b2494025f0991fc2dbce21327a9ce2fdd25fd
Peter-Oenga <poenga230@gmail.com>
2023-06-26 06:29:56 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-  *
-  *reset_to_98 - Changes the value to 98
-  *
-  *Return: Empty
+  * reset_to_98 - Display a new value
+  * @n: Pointer to memory address
+  * Return: Empty
   */
 void reset_to_98(int *n)
 {
