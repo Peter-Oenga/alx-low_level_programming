b68bf04afb710e2319b49ad08e784414769add5d
Peter-Oenga <poenga230@gmail.com>
2023-06-26 06:48:26 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * main - check the code
+  *Return: 0 Always (Success)
+  *
+  */
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+	printf("a=%d, b=%d\n", a, b);
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *swap_int - Swaps the values of a and b
+  *@a: The pointer to int a
+  *@b:Pointer to integer b
+  *Return: Nothing
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
