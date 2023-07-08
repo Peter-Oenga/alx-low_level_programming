7009e0880e99be0c79099edbd8a1da533d3864c7
Peter-Oenga <poenga230@gmail.com>
2023-07-08 05:34:38 +0300

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Escribir un texto
+ *
+ * @str: This is my entry
+ *
+ */
+
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strspn - This is my function
+ * @s: This is the string literal
+ * @accept: This is the second string
+ *
+ * Return: This is the equal string to S
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+}
