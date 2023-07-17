0380276b8f0f25213e832c2bd48d575eae0bd75c
Peter-Oenga <poenga230@gmail.com>
2023-07-17 14:56:41 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - Dog attributes
+  * @name: Name of the dog
+  * @age: Dog age
+  * @owner: Dog owner
+  * Struct dog with name, age and owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
